b4e20da7981879d4363d7ac575be8cf40add52c5
unknown <heroidorin@gmail.com>
2022-04-03 00:14:19 +0300

    Question in first 3 paragraphs

--- unkparse.py
+++ unkparse.py
@@ -5,30 +5,35 @@ import transformers
 
 # QUESTION = "Who discovered penicillin?"
 QUESTION = "When did Hitler die?"
-# STRING GOL LA QUESTIOn
+# QUESTION_LIST = ["Who became the first king of a united Italy in 1861?", "Who discovered penicillin?",
+#                  "Where is the northernmost point of land in the world?"]
+# QUESTION_LIST = ["Who discovered penicillin?"]
+
+# STRING GOL LA QUESTION
 
 question_answerer = transformers.pipeline('question-answering')
 slist = list(search(QUESTION, num_results=6, lang="en"))
-print(slist)
-print(len(slist))
-exit(0)
 for _link in slist:
     link = requests.get(_link)
-    print(link.status_code)
+    print(_link)
+    # print(link.status_code)
     if link.status_code == 200:  # some sites return 403
         break
 
+with open("link-content.html", "w") as f:
+    f.write(link.content.decode("utf-8"))
 soup = bs(link.content, "html.parser")
 allp = soup.find_all("p")
 
 f = open("f.html", "w")
+print(QUESTION)
 answer_list = []
+idx = 0
 for idx, p in enumerate(allp):
     if idx > 3:
         break
-    print(p)
     try:
-        print("Processing..")
+        print(p)
         answer_list.append(question_answerer({
             'question': QUESTION,
             'context': p.text
