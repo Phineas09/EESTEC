8277917bb18c178770fc661706641756779f46e3
Phineas09 <claudiu.ghenea15@yahoo.ro>
2022-04-02 12:39:59 +0300

    Added some functionality
    Added the sanity check response and an route '/test/echoEndpoint' for
    testing the app 'POST' functionality and error handling
    
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ Project uses Python 3.8.10
 In orded to enter the virtual environment just run
 ```bash
 source .venv/bin/activate
+```
 
+In order to install the requirements from the 'requirements.txt' just run
+
+```bash
+pip install -r requirements.txt
 ```
 
 
--- server.py
+++ server.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
+''' 
+        Sanity check, returns 200 and empty body, it proves that the server is 
+    running
+'''
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! test test</p>"
+
+@app.route("/sanity", methods=['GET'])
+def sanityResponse():
+    return Response("", status=200, mimetype='application/json')
+
+
+'''
+        Echo response test for path /test/echoEndpoint, it returns the sent 
+    json, if none present must return error
+'''
+
+
+@app.route('/test/echoEndpoint', methods=['POST'])
+def my_test_endpoint():
+    data = request.get_json()
+    if data is None:
+        abort(400)
+    return jsonify(data)
+
+
+'''
+        The bellow section is for errorhandlers
+'''
+
+
+@app.errorhandler(400)
+def page_not_found():
+    return Response('No data provided! It should be of type application/json',
+                    status=400,
+                    mimetype='application/json')
+
+
+'''
+        Main function for starting the app
+'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
